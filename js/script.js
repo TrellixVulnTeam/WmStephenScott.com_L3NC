19208787217f4b6f53b3a05d96896594407d30f6
SteveScott <Stevo517@William-Scotts-MacBook.local>
2014-02-26 13:12:45 -0500

    JavaScript Aside Buttons
    Implemented fading on side buttons

--- js/script.js
+++ js/script.js
@@ -7,5 +7,26 @@ $(document).ready(function(){
     $WSSText.fadeIn(3000);
     $WSSBackground.fadeIn(3000);
     $AsideText.delay(2000).fadeIn(3000);
-    
+
+    var mySkills = new Array();
+    mySkills[0] = 'GIS';
+    mySkills[1] = 'UrbanDesign';
+    mySkills[2] = 'Architecture';
+    mySkills[3] = '3DModeling';
+
+	function FadeMe(skill)
+		{
+		    $('h2#'+skill).fadeTo("fast", .25)
+		    $('h2#'+skill).mouseenter(function(){
+		    	$('h2#'+skill).fadeTo(400, 1)});
+
+		    $('h2#'+skill).mouseleave(function(){
+		    	$('h2#'+skill).fadeTo(400,0.25)
+			});
+		};
+	FadeMe(mySkills[0])
+	FadeMe(mySkills[1])
+	FadeMe(mySkills[2])
+	FadeMe(mySkills[3])
+
 });
