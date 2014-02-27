9cecae9e54f1a4736e1efd43ee7e237f38fda178
SteveScott <Stevo517@William-Scotts-MacBook.local>
2014-02-26 22:48:00 -0500

    fuckingWork!

--- js/script.js
+++ js/script.js
@@ -15,10 +15,27 @@ $(document).ready(function(){
     mySkills[3] = '3DModeling';
 
 	function FadeMe(skill)
-		{
+		{	
+			//$('.destroyMe').remove();
+
+			//var element = document.getElementById('popupText');
+			//if (element == null) {;}
+			//else {element.parentNode.removeChild(element);};
+
+		//	$('#contentColumn').removeChild('popupText')
+		 //	$('#imageColumn').removeChild('popupImage')
+
 		    $('h2#'+skill).fadeTo("fast", .25)
 		    $('h2#'+skill).mouseenter(function(){
-		    	$('h2#'+skill).fadeTo(400, 1)});
+		    	$('h2#'+skill).fadeTo(400, 1);
+		    	$('div#contentColumn').empty();
+				$('div#imageColumn').empty();
+		    	var text = "<p id='popupText' class='destroyMe'>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>"
+		    	$(text).hide().appendTo($('#contentColumn')).fadeIn(1000);
+
+		    	var image = "<img id='popupImage' class='destroyMe' src='img/closeup.png' alt='nikken' width='500px'>"
+		    	$(image).hide().appendTo($('#imageColumn')).fadeIn(1000);
+		    	});
 
 		    $('h2#'+skill).mouseleave(function(){
 		    	$('h2#'+skill).fadeTo(400,0.25)
@@ -28,5 +45,7 @@ $(document).ready(function(){
 	FadeMe(mySkills[1])
 	FadeMe(mySkills[2])
 	FadeMe(mySkills[3])
+
+
 
 });
